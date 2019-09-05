96ce5742e3fa1d120a090831e2b3a993bcc62cee
Noor Al Darraji <077068@yahoo.com>
2019-09-05 16:07:18 -0500

    Created custom middleware for user ID :hammer:

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter.js')
 const server = express();
+
+
+
+server.use(logger)
+server.use('/users', userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +15,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${new Date()} | Method -> ${req.method} | URL -> ${req.url}`)
+  next();
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,8 @@
-const express = 'express';
+const express = require('express');
+const userDb = require('./userDb')
 
 const router = express.Router();
+router.use(express.json());
 
 router.post('/', (req, res) => {
 
@@ -11,11 +13,18 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    userDb.get()
+        .then(results => {
+            res.status(200).json(results)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.user)
+    res.send(`<h2>ok this is id</h2>`)
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -33,6 +42,17 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const userId = req.params.id;
+    userDb.getById(userId)
+        .then(results => {
+            console.log(results)
+            if (results === undefined) {
+                res.status(400).json({ message: "Invalid user ID" })
+            } else {
+                req.user = results
+                next();
+            }
+        })
 
 };
 
